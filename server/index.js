b39e55dd20322ad74d07d9916fd09bd40fc670d8
Kiia Silvonen <silvonenkiia@gmail.com>
2024-11-11 17:35:17 +0200

    part 6

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import {router as todoRouter} from './routers/todoRouter.js';
+import {router as userRouter} from './routers/userRouter.js';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use('/',todoRouter)
+app.use('/user',userRouter)
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({error: err.message})
+})
+
+app.listen(port);
